627ef5c16d07774b55c2e8045908211d16451ce4
Hang8s <pidcerkovnijdmitro@gmail.com>
2025-10-11 23:42:03 +0300

    end crud

--- awesome_by_guide/settings.py
+++ awesome_by_guide/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b2i0-4gwiypv*#pc=&d39&_6=4(-gbgpbe8kif!*@u+xwsy$cc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render , redirect , get_object_or_404
 from .models import *
-from django.forms import ModelForm
-from django import forms
 from django.contrib import messages
-
+from .forms import *
 from bs4 import BeautifulSoup
 import requests
 
@@ -11,16 +9,6 @@ import requests
 def home_view(request):
     posts = Post.objects.all()
     return render(request,'posts/home.html',{'posts':posts})
-
-class PostCreateForm(ModelForm):
-    class Meta:
-        model = Post
-        fields = ['url', 'body']
-        labels = {'body' : 'Caption',}
-        widgets = {
-            'body' : forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a caption ...', 'class': 'font1 text-4xl'}),
-            'url' : forms.TextInput(attrs={'placeholder': 'Add url ...'}),
-        }
 
 def post_create_view(request):
     form = PostCreateForm()
@@ -53,10 +41,31 @@ def post_create_view(request):
 
 
 def post_delete_view(request,pk):
-    post = Post.objects.get(id=pk)
+    post = get_object_or_404(Post, id=pk)
     if request.method=='POST':
         post.delete()
         messages.success(request,'Post deleted')
         return redirect('home')
     return render(request,'posts/post_delete.html',{'post':post})
 
+def post_edit_view(request,pk):
+    post = get_object_or_404(Post, id=pk)
+    form = PostEditForm(instance=post)
+    context = {
+        'post':post,
+        'form':form
+    }
+    if request.method =='POST':
+        form = PostEditForm(request.POST,instance=post)
+        if form.is_valid:
+            form.save()
+            messages.success(request,'Post Updated')
+            return redirect('home')
+        
+        
+        
+    return render(request , 'posts/post_edit.html',context)
+
+def post_page_view(request,pk):
+    post = get_object_or_404(Post, id=pk)
+    return render(request,'posts/post_page.html',{'post':post})
