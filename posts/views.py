f56583b972c3b5781dce9c445710be31f58b5c18
Hang8s <pidcerkovnijdmitro@gmail.com>
2025-10-21 23:28:40 +0300

    delete users view and other

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render , redirect , get_object_or_404
 from .models import *
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import *
 from bs4 import BeautifulSoup
@@ -23,7 +24,7 @@ def home_view(request,tag=None):
     
     return render(request,'posts/home.html',context)
 
-
+@login_required
 def post_create_view(request):
     form = PostCreateForm()
     
@@ -47,6 +48,7 @@ def post_create_view(request):
             post.artist = artist
             
             post.author = request.user
+
             
             post.save()
             form.save_m2m()
@@ -54,17 +56,18 @@ def post_create_view(request):
     
     return render(request, 'posts/post_create.html', {'form' : form })
 
-
+@login_required
 def post_delete_view(request,pk):
-    post = get_object_or_404(Post, id=pk)
+    post = get_object_or_404(Post, id=pk,author=request.user)
     if request.method=='POST':
         post.delete()
         messages.success(request,'Post deleted')
         return redirect('home')
     return render(request,'posts/post_delete.html',{'post':post})
 
+@login_required
 def post_edit_view(request,pk):
-    post = get_object_or_404(Post, id=pk)
+    post = get_object_or_404(Post, id=pk,author=request.user)
     form = PostEditForm(instance=post)
     context = {
         'post':post,
