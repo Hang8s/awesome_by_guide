053c98c37fd354bf0f306fc35b6769d7332221a3
Hang8s <pidcerkovnijdmitro@gmail.com>
2025-10-22 00:23:37 +0300

    add onboarding and python signals

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect,get_object_or_404
+from django.urls import reverse
 from django.http import Http404
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
@@ -27,8 +28,13 @@ def profile_edit_view(request):
         if form.is_valid():
             form.save()
             return redirect('profile')
+        
+    if request.path == reverse('profile-onboarding'):
+        template = 'users/profile_onboarding.html'
+    else:
+        template = 'users/profile_edit.html'
     
-    return render(request,'users/profile_edit.html',{'form':form})
+    return render(request,template,{'form':form})
 
 @login_required
 def profile_delete_view(request):
