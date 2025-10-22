9ad3fa7b5d0ad742af220c2ed514004bf8aad99d
Hang8s <pidcerkovnijdmitro@gmail.com>
2025-10-22 23:18:37 +0300

    starting of add comments now added model and showing in template

--- posts/models.py
+++ posts/models.py
@@ -32,3 +32,17 @@ class Tag(models.Model):
     
     class Meta:
         ordering = ['order'] 
+        
+class Comment(models.Model):
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,related_name='comments')
+    parent_post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    body = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.CharField(max_length=100,default=uuid.uuid4,unique=True,primary_key=True,editable=True)
+    
+    def __str__(self):
+        try:
+            return f'{self.author.username} : {self.body[:30]}'
+        except:
+            return f'no author : {self.body[:30]}'
+            
