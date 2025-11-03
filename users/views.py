a65b34e1c636316aab5c42b8298b0326984f6337
Hang8s <pidcerkovnijdmitro@gmail.com>
2025-11-03 18:08:52 +0200

    profile tabs

--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect,get_object_or_404
 from django.urls import reverse
+from django.db.models import Count
 from django.http import Http404
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
+from posts.forms import ReplyCreateForm
 from .forms import *
 
 
@@ -19,6 +21,17 @@ def profile_view(request,username=None):
             raise Http404()
     posts = profile.user.posts.all()
     
+    if request.htmx:
+        if 'top-posts' in request.GET:
+            posts = profile.user.posts.annotate(num_likes=Count('likes')).filter(num_likes__gt=0).order_by('-num_likes')
+        elif 'top-comments' in request.GET:
+            comments = profile.user.comments.annotate(num_likes=Count('likes')).filter(num_likes__gt=0).order_by('-num_likes')
+            replyform = ReplyCreateForm()
+            return render(request,'snippets/loop_profile_comments.html',{'comments':comments,'replyform':replyform})
+        elif 'liked-posts' in request.GET:
+            posts = profile.user.likedposts.order_by('-likedpost__created')
+        return render(request,'snippets/loop_profile_posts.html',{'posts':posts})
+            
     context ={
         'profile':profile,
         'posts':posts
